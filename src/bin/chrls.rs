ed6f0fdb0957d46d2f944957bcf70a95559c913e
陈浩然 <chenhr13@chinatelecom.cn>
2025-12-19 16:32:12 +0800

    [Modify] 修改chrls命令
    之前的chrls命令可以列出无rx权限的目录，已做修改

--- src/bin/chrls.rs
+++ src/bin/chrls.rs
@@ -3,6 +3,7 @@ use std::fs;
 use std::io;
 use std::path::Path;
 use std::process;
+use std::os::unix::fs::PermissionsExt;
 
 /// 打印用法
 fn print_usage() {
@@ -13,6 +14,7 @@ fn print_usage() {
 fn chrls(path: &Path) -> io::Result<()> {
     let entries = fs::read_dir(path)?;
 
+    ensure_can_ls(path)?;
     for entry in entries {
         let entry = entry?;
         let file_name = entry.file_name();
@@ -20,6 +22,31 @@ fn chrls(path: &Path) -> io::Result<()> {
         // Linux 文件名可能不是 UTF-8
         let name = file_name.to_string_lossy();
         println!("{}", name);
+    }
+
+    Ok(())
+}
+
+// 确保目录可读且可执行
+fn ensure_can_ls(path: &Path) -> io::Result<()> {
+    let meta = fs::metadata(path)?;
+    if !meta.is_dir() {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            "Not a directory",
+        ));
+    }
+
+    let mode = meta.permissions().mode();
+
+    let has_r = mode & 0o444 != 0;
+    let has_x = mode & 0o111 != 0;
+
+    if !has_r || !has_x {
+        return Err(io::Error::new(
+            io::ErrorKind::PermissionDenied,
+            "Permission denied",
+        ));
     }
 
     Ok(())
